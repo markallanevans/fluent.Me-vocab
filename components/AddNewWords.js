73969fa898e94c1a73445b762ba10eec5d96868c
maktubhelou <markallanevans@gmail.com>
2018-06-14 17:53:08 +0700

    add word feature works

--- components/AddNewWords.js
+++ components/AddNewWords.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text, TextInput, Image, StyleSheet, ScrollView } from 'react-native';
 import { $secondaryWhite, $secondaryRed, $primaryRed, $primaryWhite } from '../styles/styles';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import styles from '../styles/styles';
 
@@ -33,55 +34,85 @@ const boxStyles = StyleSheet.create({
   },
 });
 
-const AddWords = ({ navigation }) => (
-  <ScrollView>
-  <View style={styles.container}>
-    <View style={{height: 50}} />
-    <Text style={styles.header}>Add new Words</Text>
-    <Text style={{color: $primaryWhite, fontSize: 18, padding: 10}}>Add your own vocabulary to any category, or create a new category.</Text>
-    <View style={{margin: 10}}>
-      <TextInput
-        placeholder="your word..."
-        placeholderTextColor={$placeHolderBlue}
-        style={boxStyles.newWordBox}
-        />
-    </View>
-    <View style={{margin: 10}}>
-      <TextInput
-        placeholder="your sentence..."
-        placeholderTextColor={$placeHolderBlue}
-        style={boxStyles.newSentenceBox}
-        />
-    </View>
-    <Text style={{margin: 10, color: 'white', fontSize: 18 }} >Suggested Image:</Text>
+class AddWords extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      wordToAdd: 'empty',
+      sentenceToAdd: '',
+      category: this.props.currentCategory,
+    };
+  }
+  
+  render() {
+    console.log(JSON.stringify(this.state));
+    return (
+      <ScrollView>
+        <View style={styles.container}>
+          <View style={{ height: 50 }} />
+          <Text style={styles.header}>Add new Words</Text>
+          <Text style={{ color: $primaryWhite, fontSize: 18, padding: 10 }}>Add your own vocabulary to any category, or create a new category.</Text>
+          <View style={{ margin: 10 }}>
+            <TextInput
+              placeholder="your word..."
+              placeholderTextColor={$placeHolderBlue}
+              style={boxStyles.newWordBox}
+              onEndEditing={e => this.setState({ wordToAdd: e.nativeEvent.text.toLowerCase() })}
+              />
+          </View>
+          <View style={{ margin: 10 }}>
+            <TextInput
+              placeholder="your sentence..."
+              placeholderTextColor={$placeHolderBlue}
+              style={boxStyles.newSentenceBox}
+              onEndEditing={e => this.setState({ sentenceToAdd: e.nativeEvent.text })}
+              />
+          </View>
+          <Text style={{ margin: 10, color: 'white', fontSize: 18 }} >Suggested Image:</Text>
 
-    <View style={styles.wordBoxEng}>
-      <Image
-        source={{ uri: `https://source.unsplash.com/150x150/?${searchWord}` }}
-        style={styles.imageSize} 
-        />
-    </View>
-    <View style={{margin: 10}}>
-      <TextInput
-        placeholder="category"
-        placeholderTextColor={$placeHolderBlue}
-        style={styles.answerBox}
-      />
-    </View>
-    <View style={styles.button}>
-      <Text
-        style={styles.buttonText}
-        onPress={() => navigation.navigate('Vocab')}
-      >Add!
-      </Text>
-    </View>
-    <View style={{height: 50}} />
-  </View>
-  </ScrollView>
-);
+          <View style={styles.wordBoxEng}>
+            <Image
+              source={{ uri: `https://source.unsplash.com/150x150/?${this.state.wordToAdd && this.state.wordToAdd}` }}
+              style={styles.imageSize}
+            />
+          </View>
+          <View style={{ margin: 10 }}>
+            <TextInput
+              placeholder={this.props.currentCategory}
+              placeholderTextColor={$placeHolderBlue}
+              style={styles.answerBox}
+            />
+          </View>
+          <View style={styles.button}>
+            <Text
+              style={styles.buttonText}
+              onPress={() => this.props.addNewWordToStore(this.state.wordToAdd, this.state.sentenceToAdd, this.state.category)}
+            >Add!
+            </Text>
+          </View>
+          <View style={{ height: 50 }} />
+        </View>
+      </ScrollView>
+    );
+  }
+}
 
 AddWords.propTypes = {
   navigation: PropTypes.object.isRequired,
 };
 
-export default AddWords;
+const mapDispatchToProps = dispatch => ({
+  addNewWordToStore: (word, sentence, category) => dispatch({
+    type: 'ADD_NEW_WORD_TO_STORE',
+    word,
+    sentence,
+    category,
+  }),
+});
+
+
+const mapStateToProps = state => ({
+  currentCategory: state.sentenceStore.currentCategoryTitle,
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(AddWords);
--- redux/sentenceStore.js
+++ redux/sentenceStore.js
@@ -32,6 +32,36 @@ const sentenceStore = (state = {
         words: Object.values(loadedCategory)[currentWordIndex].split(' '),
       };
     }
+    case 'ADD_NEW_WORD_TO_STORE': {
+      const { allCategories, loadedCategory } = state;
+      const categories = Object.keys(allCategories);
+      categories.forEach((category) => {
+        console.log('updating category ', category)
+        allCategories[category].listWords = allCategories[category].listWords.concat(action.word);
+        if ('sentences' in allCategories[category]) allCategories[category].sentences[action.word] = action.sentence;
+        console.log(allCategories[category])
+      })
+
+      return {
+        ...state,
+        allCategories,
+      }
+
+      // return {
+      //   ...state,
+      //   allCategories = {
+      //     ...allCategories[action.category].listWords, listWords.concat([action.word]),
+      //     ...allCategories[action.category].sentences: allCategories[action.category].sentences.[action.word]: {action.sentence} // add word and sentence
+      //   },
+      //   categoryWords: Object.keys(loadedCategory),
+      //   loadedSentence: Object.values(loadedCategory)[currentWordIndex],
+      //   words: Object.values(loadedCategory)[currentWordIndex].split(' '),
+      // };
+    }
+
+    // object.categories.Animals = { ...object.categories.Animals, snakes: "snakes suck too" };
+
+
     case 'CHECK_SENTENCE':
       return state;
     default:
