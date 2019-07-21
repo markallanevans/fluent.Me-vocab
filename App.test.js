1b278524d574219e15c24d18de5acdb30eeb7d70
Mark Evans <markallanevans@gmail.com>
2019-07-21 21:45:01 +0700

    Set up and configured jest

---
+++ App.test.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+
+import App from './App'
+
+describe('<App />', () => {
+  it('should render without crashing', () => {
+    const tree = renderer.create(<App />).toJSON()
+  })
+})
