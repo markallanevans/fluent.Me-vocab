7304e1870caa039bc283d3eb8caa2d19768592f9
Mark Evans <markallanevans@gmail.com>
2019-07-22 19:10:57 +0700

    Fixed babel config bug

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,8 @@
-module.exports = (api) => {
-    api.cache(true)
-  
-    return {
-      presets: ['module:metro-react-native-babel-preset'],
-      plugins: [
-        '@babel/plugin-transform-runtime',
-        //TODO: Temporarily remove below until fixing related bug
-        // '@babel/plugin-proposal-class-properties',
-      ].map(require.resolve),
-    }
+module.exports = api => {
+  api.cache(true)
+
+  return {
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: ['@babel/plugin-transform-runtime']
   }
+}
