b35df2273cd358e44e56dec851f52222352a83c8
Mark Evans <mark.evans@nfq.asia>
2019-07-22 16:02:44 +0700

    Fix bug with plugin-proposal-class-properties

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,12 @@
-module.exports = api => {
-  api.cache(true)
-
-  return {
-    presets: ['module:metro-react-native-babel-preset'],
-    plugins: ['@babel/plugin-proposal-class-properties']
+module.exports = (api) => {
+    api.cache(true)
+  
+    return {
+      presets: ['module:metro-react-native-babel-preset'],
+      plugins: [
+        '@babel/plugin-transform-runtime',
+        //TODO: Temporarily remove below until fixing related bug
+        // '@babel/plugin-proposal-class-properties',
+      ].map(require.resolve),
+    }
   }
-}
